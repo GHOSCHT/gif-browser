139b008a9f56d3bf2780f50ba6d8290aa649a807
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-06-30 20:41:00 +0200

    Add darkmode

--- app/index.js
+++ app/index.js
@@ -7,6 +7,7 @@ import { AppContainer as ReactHotAppContainer } from "react-hot-loader";
 import Home from "./components/Home";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import NavBar from "./components/NavBar";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
@@ -15,12 +16,23 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: "#212121",
+    },
+  },
+});
+
 render(
   <AppContainer>
     <Provider store={store}>
-      <CssBaseline />
-      <NavBar />
-      <Home />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <NavBar />
+        <Home />
+      </ThemeProvider>
     </Provider>
   </AppContainer>,
   document.getElementById("root")
