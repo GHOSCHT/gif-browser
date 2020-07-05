96f67e2a315df2bd8c0ef0aaae3851b317a02247
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-07-05 14:24:54 +0200

    Update ImageCard.js

--- app/components/ImageCard.js
+++ app/components/ImageCard.js
@@ -1,19 +1,16 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     width: "345px",
-    marginTop: "20px",
-    marginLeft: "10px",
-    marginRight: "10px",
+    marginTop: "10px",
   },
   media: {
     height: "345px",
@@ -36,7 +33,7 @@ function downloadImage(url) {
   const dialogOptions = {
     title: "Save GIF",
     filters: [
-      { name: "Images", extensions: ["gif"] },
+      { name: "Image", extensions: ["gif"] },
       { name: "All Files", extensions: ["*"] },
     ],
   };
